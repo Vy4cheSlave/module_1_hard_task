56edf60b17540c94d523c5514a929fcd3e318b59
Vy4cheSlave <slav.subocheff@yandex.ru>
2025-06-14 10:55:02 +0700

    feat: solve tasks

--- fan_in/merge_channels.go
+++ fan_in/merge_channels.go
@@ -1,9 +1,31 @@
 package fan_in
 
+import (
+	"sync"
+)
+
 // MergeChannels - принимает несколько каналов на вход и объединяет их в один
 // Fan-in и merge channels синонимы
 func MergeChannels(channels ...<-chan int) <-chan int {
 	// TODO: ваш код
+	outputChan := make(chan int)
+	wg := &sync.WaitGroup{}
+
+	for _, channel := range channels {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for value := range channel {
+				outputChan <- value
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(outputChan)
+	}()
+	return outputChan
 
 	panic("implement me")
 }
--- generate_password/generate_password.go
+++ generate_password/generate_password.go
@@ -1,10 +1,27 @@
 package generate_password
 
+import (
+	"math/rand"
+	"bytes"
+	"time"
+)
+
 // letters - список допустимых символов в пароле
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func GeneratePassword(n int) (string, error) {
 	// TODO: ваш код
+	rand.Seed(time.Now().UnixNano())
+	indexLen := len(letters)
+	var buf bytes.Buffer
+	var randVal int
+	for i := 0; i < n; i++ {
+		randVal = rand.Intn(indexLen)
+		buf.WriteByte(letters[randVal])
+	}
+
+	return buf.String(), nil
+
 
 	panic("implement me")
 }
